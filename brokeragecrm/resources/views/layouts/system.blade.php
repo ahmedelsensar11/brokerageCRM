3e4a1f0f7889e0d397781778a07d77263b5a598c
ahmelsens11 <ahmelsens11@gmail.com>
2020-12-13 11:26:36 +0200

    the totaly main project

--- brokeragecrm/resources/views/layouts/system.blade.php
+++ brokeragecrm/resources/views/layouts/system.blade.php
@@ -45,7 +45,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
+        <a href="{{url('/home')}}" class="nav-link">Home</a>
       </li>
       
     </ul>
@@ -194,16 +194,6 @@
 
           </li>
 
-          <!-- Charts -->
-          <li class="nav-item has-treeview">
-            <a href="{{url('#')}}" class="nav-link">
-              <i class="nav-icon text-danger fas fa-chart-pie"></i>
-              <p>
-                Charts
-              </p>
-            </a>
-
-          </li>
 
         </ul>
       </nav>
