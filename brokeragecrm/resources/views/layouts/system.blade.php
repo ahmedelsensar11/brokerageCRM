ee79da2b530d3df1ecf7b1deb81d4c9e6806319b
ahmelsens11 <ahmelsens11@gmail.com>
2020-12-13 11:19:17 +0200

    the totaly main project

--- brokeragecrm/resources/views/layouts/system.blade.php
+++ brokeragecrm/resources/views/layouts/system.blade.php
@@ -58,9 +58,7 @@
           {{Auth::user()->first_name}}
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Profile</a>
-
-          <div class="dropdown-divider"></div>
+          
           <a class="dropdown-item" href="{{ route('logout') }}"
               onclick="event.preventDefault();
                             document.getElementById('logout-form').submit();">
